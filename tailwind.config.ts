3107c3043437d7586adf1e2b66729ad9a82a55ae
Fatih <fatihulil@gmail.com>
2025-01-08 08:00:19 +0700

    Update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, and tailwindcss-animate to package.json.
    - Updated tailwind.config.ts to include new color variables and improved theme settings for better customization.
    - Enhanced global styles in globals.css to define CSS variables for light and dark themes.
    - Modified search.json to ensure proper formatting and maintain consistency in blog post data.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import animate from "tailwindcss-animate";
+
+/** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     './node_modules/pliny/**/*.js',
@@ -16,13 +22,56 @@ export default {
         13: '3.25rem',
         14: '3.5rem',
       },
+      // colors: {
+      //   primary: colors.pink,
+      //   gray: colors.gray,
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
       colors: {
-        primary: colors.pink,
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        // primary: colors.pink,
         gray: colors.gray,
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          ...colors.pink,
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [forms, typography, animate],
 } satisfies Config;
